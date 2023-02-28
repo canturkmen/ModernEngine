92e3ffcd4c1e3e0fe875475a3607c3698d179fd9
Can Türkmen <turkmenc098@gmail.com>
2023-02-28 14:07:58 +0300

    Loaded glyph geometry data that our fonts can support. (191 out of 224)

--- ModernEngine/src/ModernEngine/Renderer/Font.cpp
+++ ModernEngine/src/ModernEngine/Renderer/Font.cpp
@@ -3,31 +3,73 @@
 
 #undef INFINITE
 #include "msdf-atlas-gen.h"
+#include "GlyphGeometry.h"
 
 namespace ModernEngine {
 
+	struct MSDFData
+	{
+		std::vector<msdf_atlas::GlyphGeometry> Glyphs;
+		msdf_atlas::FontGeometry FontGeometry;
+	};
+
 	Font::Font(const std::filesystem::path& filePath)
+		: m_Data(new MSDFData())
 	{
 		msdfgen::FreetypeHandle* ft = msdfgen::initializeFreetype();
 		if (ft) {
 			std::string sFilePath = filePath.string();
 			msdfgen::FontHandle* font = msdfgen::loadFont(ft, sFilePath.c_str());
-			if (font) {
-				msdfgen::Shape shape;
-				if (msdfgen::loadGlyph(shape, font, 'C')) {
-					shape.normalize();
-					//                      max. angle
-					msdfgen::edgeColoringSimple(shape, 3.0);
-					//           image width, height
-					msdfgen::Bitmap<float, 3> msdf(32, 32);
-					//                     range, scale, translation
-					msdfgen::generateMSDF(msdf, shape, 4.0, 1.0, msdfgen::Vector2(4.0, 4.0));
-					msdfgen::savePng(msdf, "output.png");
-				}
-				msdfgen::destroyFont(font);
+
+			if (!font) 
+			{ 
+				MN_CORE_ERROR("Failed to load font {}", sFilePath);
+				return;
 			}
+
+			struct CharsetRange
+			{
+				uint32_t Begin, End;
+			};
+
+			static const CharsetRange charsetRanges[] =
+			{
+				{0x0020, 0x00FF}
+			};
+			
+			msdf_atlas::Charset charset;
+			for (CharsetRange range : charsetRanges)
+			{
+				for (uint32_t c = range.Begin; c <= range.End; c++)
+					charset.add(c);
+			}
+
+			double fontScale = 1.0f;
+			m_Data->FontGeometry = msdf_atlas::FontGeometry(&m_Data->Glyphs);
+			int glyphsLoaded = m_Data->FontGeometry.loadCharset(font, fontScale, charset);
+			MN_CORE_INFO("Loaded {} glyphs from font (out of {})", glyphsLoaded, charset.size());
+#if 0
+			msdfgen::Shape shape;
+			if (msdfgen::loadGlyph(shape, font, 'C')) {
+				shape.normalize();
+				//                      max. angle
+				msdfgen::edgeColoringSimple(shape, 3.0);
+				//           image width, height
+				msdfgen::Bitmap<float, 3> msdf(32, 32);
+				//                     range, scale, translation
+				msdfgen::generateMSDF(msdf, shape, 4.0, 1.0, msdfgen::Vector2(4.0, 4.0));
+				msdfgen::savePng(msdf, "output.png");
+			}
+#endif
+			
+			msdfgen::destroyFont(font);
 			msdfgen::deinitializeFreetype(ft);
 		}
+	}
+
+	Font::~Font()
+	{
+		delete m_Data;
 	}
 }
 
--- ModernEngine/src/ModernEngine/Renderer/Font.h
+++ ModernEngine/src/ModernEngine/Renderer/Font.h
@@ -1,11 +1,19 @@
 #pragma once
 
+#include <filesystem>
+#include "ModernEngine/Core/Base.h"
+
 namespace ModernEngine {
+
+	struct MSDFData;
+
 	class Font
 	{
 	public:
 		Font(const std::filesystem::path& filePath);
+		~Font();
 
 	private:
+		MSDFData* m_Data;
 	};
 }
